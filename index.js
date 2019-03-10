d5f9a3f7cdb0a8881977420c921f3c528c878975
nguyenxuanhungst <48404698+nguyenxuanhungst@users.noreply.github.com>
2019-03-10 17:02:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ app.get('/', function(request, response) {
 login({email: "0584176294", password: "1621997"}, (err, api) => {
     if(err) return console.error(err);
 
+    api.sendTypingIndicator(message.threadID);
+    
     api.listen((err, message) => {
         api.sendMessage(message.body, message.threadID);
+        api.sendTypingIndicator(message.threadID);
     });
-    api.sendTypingIndicator(message.threadID);
+    
 
 });
