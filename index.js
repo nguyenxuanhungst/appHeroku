225077d028f0a8fd1de54c4fb2e8a46cb4534ad8
NGUYEN HUNG <nguyenxuanhungst@gmail.com>
2019-03-10 15:47:54 +0700

    make it better 2

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const login = require("facebook-chat-api");
+//
+var express = require('express');
+var app     = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 // Create simple echo bot
 login({email: "0584176294", password: "1621997"}, (err, api) => {
@@ -7,7 +20,5 @@ login({email: "0584176294", password: "1621997"}, (err, api) => {
     api.listen((err, message) => {
         api.sendMessage(message.body, message.threadID);
     });
-api.listen((err, message) => {
-        api.sendMessage(message.body, message.threadID);
-    });
+
 });
